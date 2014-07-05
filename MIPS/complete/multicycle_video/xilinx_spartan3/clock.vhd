207941a0be83b8730070c1b27378117d8aeca345
rafaelcaporal@gmail.com <rafaelcaporal@gmail.com@5bda5ab7-cda1-41da-1735-5580e44126ff>
2014-07-05 02:43:41 +0000

    Arquivos necessários para sintetização dos códigos vhd na placa spartan 3

---
+++ MIPS/complete/multicycle_video/xilinx_spartan3/clock.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all ;
+
+entity clock is port (
+clk: in std_logic;
+clk_out: buffer std_logic);
+end clock;
+
+architecture behaviour of clock is
+
+signal reset : std_logic;
+
+begin
+process(clk,reset)
+begin
+
+if reset = '1' then
+clk_out <= '0';
+elsif rising_edge(clk) then
+clk_out <= not clk_out;
+end if;
+end process;
+
+end behaviour;
